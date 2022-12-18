3bd172677b20610f90d6064de45c0ba2f698cec4
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-12-18 01:05:53 -0400

    Add files via upload

---
+++ Arrays.js/Array-LinkedList Hybrid.js
@@ -0,0 +1,10 @@
+/**
+a linked list with the pros of an array.
+
+Best use-case would be for https://llamalab.com/automate/community/flows/38232 ,
+because it would allow O(1) searching of nearest non-`null` values
+*/
+class LinkArray extends Array {
+	constructor(...x) {super(...x)}
+
+}
