01021ef6ff469f857c4588f7a915960d29a4e498
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-12-20 23:22:19 -0400

    Delete Unbounded Array.js

--- Arrays.js/Unbounded Array.js
+++
@@ -1,9 +0,0 @@
-/**
-- Uses BigInts as indices
-- has no empty slots, only `undefined` slots
-- despite not having empty slots, it's internally sparse, therefore it uses minimal memory
-- its iterator ignores all `undefined`s, for faster iteration
-*/
-class UnboundedArray {
-
-}
