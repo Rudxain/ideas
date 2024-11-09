07faf90cb601879586a4a416d382d97d7bfbf31e
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2024-11-09 05:19:36 -0400

    rename stuff in with_bf.js

--- software/evar/with_bf.js
+++ software/evar/with_bf.js
@@ -1,4 +1,26 @@
-//@ts-check
+/*
+This script intentionally mixes good and bad practices.
+Inconsistency is key!
+*/
+
+/**
+This **must** be kept minified,
+to speed-up code-gen and `eval`.
+*/
+const bf_ops_to_js = Object.freeze(new Map(/**@type {const}*/([
+	['+', 'm[p]++;'],
+	[',', 'm[p]=yield;'],
+	['-', 'm[p]--;'],
+	['.', 'yield c(m[p]);'],
+	['<', 'p--;'],
+	['>', 'p++;'],
+	['[', 'while(m[p]){'],
+	[']', '}']
+])))
+
+const arab_ops_to_bf = /**@type {readonly ['+',',','-','.','<','>','[',']']}*/(
+	Object.freeze([...bf_ops_to_js.keys()])
+)
 
 /**
 Convert Arabic BrainFuck to "standard" BF.
@@ -7,51 +29,35 @@ Convert Arabic BrainFuck to "standard" BF.
 they map to BF op-codes with the same numeric order.
 @param {string} d
 */
-const arab_to_bf = d => d.replace(/./gs, c => '+,-.<>[]'[c] || '')
+const arab_to_bf = d => d.replace(/./gs, c => arab_ops_to_bf[c] || '')
 
 /**
 Convert BrainFuck to its equivalent JS
 @param {string} b
 */
-const bf_to_js = b => {
-	/**
-	This **must** be kept minified,
-	to speed-up code-gen and `eval`.
-	*/
-	const map = Object.freeze(new Map(/**@type {const}*/([
-		['+', 'm[p]++;'],
-		[',', 'm[p]=yield;'],
-		['-', 'm[p]--;'],
-		['.', 'yield c(m[p]);'],
-		['<', 'p--;'],
-		['>', 'p++;'],
-		['[', 'while(m[p]){'],
-		[']', '}']
-	])))
-
-	return `const c = String.fromCharCode
-	function* bfg(){
-		const m = new Uint8Array(3e4)
-		let p = 0;
-		${b.replace(/./gs, c => map.get(c) || '')}
-	};[...bfg()].join('')`
-}
+const bf_to_js = b => `function* bfg(){
+	const
+		c = String.fromCharCode,
+		m = new Uint8Array(3e4)
+	let p = 0;
+	${b.replace(/./gs, c => bf_ops_to_js.get(c) || '')}
+};[...bfg()].join('')` // `,` is useless, lol
 
 
 const bf = new Proxy({}, {
-	get(_, key) {
-		if (typeof key == 'symbol') return {}
-		if (key === '__global') return globalThis
-		return eval(bf_to_js(arab_to_bf(key.substring(1))))
+	get(_, k) {
+		if (typeof k == 'symbol') return {}
+		if (k === '_global') return globalThis
+		return eval(bf_to_js(arab_to_bf(k.substring(1))))
 	},
-	has(_, __) { return true }
+	has(_0, _1) { return true }
 })
 
 with (bf) {
-	__global.console.log(
-		$J_query_000000_625_000000_000000_4753 +
-		E_0000000_625_00000_00000_47523 +
-		españoL_$$0000000_625_00000_000000_47523 +
-		λ_calculus_$$$_0000000_625_00000_000000_47523
+	_global.console.log(
+		$Hquery0000006250000000000004753$$$ +
+		Español0000000625000OOO000000047523 +
+		_Lodash0000000625000000O0000047523_ +
+		λcalc00000006250000000000047523monadz
 	)
 }
